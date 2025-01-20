cf0cb6b9d0a6d30430ec39e8fd05b709b37f9770
Sajiyaking <157352890+Sajiyaking@users.noreply.github.com>
2025-01-20 17:25:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,7 @@ SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG ||"https://i.ibb.co/XZdtG0d/6254.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG ||"𝘩𝘦𝘭𝘭𝘰 𝘪 𝘢𝘮 𝘳𝘰𝘸𝘥𝘺 𝘮𝘥 𝘸𝘩𝘢𝘵𝘴 𝘢𝘱𝘱 𝘣𝘰𝘵. 𝘤𝘳𝘦𝘢𝘵𝘦 𝘣𝘺 𝘥𝘢𝘳𝘬 𝘴𝘪𝘩𝘪𝘯𝘢.",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+STATUS_READ_TEXT: process.env.STATUS_READ_TEXT || "*STATUS SEEN BY CODE HELPER REAL DEXTER",
 MODE: process.env.MODE || "inbox",
 };
