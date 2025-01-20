9583b4485cdbd0d4ba666d39677d1c6db347e7d0
Sajiyaking <157352890+Sajiyaking@users.noreply.github.com>
2025-01-20 21:57:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG ||"https://i.ibb.co/XZdtG0d/6254.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG ||"𝘩𝘦𝘭𝘭𝘰 𝘪 𝘢𝘮 𝘳𝘰𝘸𝘥𝘺 𝘮𝘥 𝘸𝘩𝘢𝘵𝘴 𝘢𝘱𝘱 𝘣𝘰𝘵. 𝘤𝘳𝘦𝘢𝘵𝘦 𝘣𝘺 𝘥𝘢𝘳𝘬 𝘴𝘪𝘩𝘪𝘯𝘢.",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-STATUS_READ_TEXT: process.env.STATUS_READ_TEXT || "*STATUS SEEN BY CODE HELPER REAL DEXTER",
-MODE: process.env.MODE || "inbox",
+ALIVE_IMG: process.env.ALIVE_IMG ||"https://telegra.ph/file/3f7249eb429c8211cbba3.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG ||"hello i am anila lochana",
 };
