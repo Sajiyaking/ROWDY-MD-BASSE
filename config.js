ffa33fec004004fa20d97ebf1acb71c5e6aefb12
dakshinamalli <Dakshinasajith95@gmail.com>
2025-02-11 19:21:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "u",
+SESSION_ID: process.env.SESSION_ID || "mY9FjALR#KKnQocWSwAzbK29G8wlpjXvxrgRX3yrNb6fb-dpROiE",
 ALIVE_IMG: process.env.ALIVE_IMG ||"https://i.ibb.co/zZ13BVL/9674.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG ||"𝐇𝐄𝐋𝐋𝐎 𝐢 𝐀𝐌 𝐑𝐎𝐖𝐃𝐘 𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓. 𝐂𝐑𝐄𝐀𝐓𝐄 𝐁𝐘 𝐃𝐀𝐑𝐊 𝐒𝐈𝐇𝐈𝐍𝐀...🌝💚 ",
 };
